f98618f482a6c1bfdaee22dc23e64769dc2d6bfb
Liam Snowdon <snowdon.liam@gmail.com>
2023-04-10 00:18:07 +0100

    Footer socials

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 const year = new Date().getFullYear()
+
+const socials = [
+  { name: 'Linkedin', icon: 'i-carbon-logo-linkedin', url: 'https://www.linkedin.com/in/liam-snowdon' },
+  { name: 'Github', icon: 'i-carbon-logo-github', url: 'https://github.com/liamsnowdon' },
+]
 </script>
 
 <template>
   <footer p="y-8 x-4 md:y-12 md:x-8">
     <Content flex="~" items="center" justify="between">
-      <span text="white sm">&copy; {{ year }}   Liam Snowdon</span>
-      <span class="i-logos-github-icon" />
+      <span text="white sm">&copy; {{ year }} Liam Snowdon</span>
+
+      <div flex="~" space="x-4">
+        <a
+          v-for="social in socials"
+          :key="social.name"
+          :href="social.url"
+          target="_blank"
+          :class="social.icon"
+          text="xl neutral-400 hover:white"
+        />
+      </div>
     </Content>
   </footer>
 </template>
