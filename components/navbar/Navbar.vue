dc8fa06642a321ac347a03c42aa102650941ea0b
Liam Snowdon <snowdon.liam@gmail.com>
2023-04-10 00:58:02 +0100

    Navbar text

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -6,7 +6,7 @@
   <nav flex="~" items="center" justify="between" p="4">
     <a href="#" flex="~" items="center" space="x-2">
       <img src="/images/memoji.png" alt="Liam Snowdon Profile Picture" h="12" w="12">
-      <span text="xl white" font="medium" m="t-3">Liam Snowdon</span>
+      <span bg="gradient-to-r gradient-from-indigo-300 gradient-to-indigo-500 clip-text" text="xl transparent" font="medium" m="t-3">Liam Snowdon</span>
     </a>
 
     <div flex="~" justify="end">
