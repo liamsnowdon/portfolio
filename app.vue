71eb1ad074e6dc906107cc9e98788b74e6342218
Liam Snowdon <snowdon.liam@gmail.com>
2023-04-09 02:29:38 +0100

    Meta data and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,57 @@
+<script setup lang="ts">
+useHead({
+  title: 'Liam Snowdon - Frontend Engineer',
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+  ],
+  meta: [
+    {
+      name: 'description',
+      content: 'I\'m a frontend engineer with over 6 years of commercial experience in web development. I have a keen eye for detail and am passionate about delivering high-quality and pixel-perfect designs.',
+    },
+    {
+      name: 'og:title',
+      content: 'Liam Snowdon - Frontend Engineer',
+    },
+    {
+      name: 'og:description',
+      content: 'I\'m a frontend engineer with over 6 years of commercial experience in web development. I have a keen eye for detail and am passionate about delivering high-quality and pixel-perfect designs.',
+    },
+    {
+      name: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'og:image',
+      content: '',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Liam Snowdon - Frontend Engineer',
+    },
+    {
+      name: 'twitter:description',
+      content: 'I\'m a frontend engineer with over 6 years of commercial experience in web development. I have a keen eye for detail and am passionate about delivering high-quality and pixel-perfect designs.',
+    },
+    {
+      name: 'twitter:image',
+      content: '',
+    },
+    {
+      name: 'twitter:image:alt',
+      content: 'Liam Snowdon - Frontend Engineer',
+    },
+  ],
+})
+</script>
+
 <template>
   <div bg="neutral-900">
     <Navbar />
