877fc3ceebf8d72de9339b0eedc060ca3f2ba29f
Liam Snowdon <snowdon.liam@gmail.com>
2023-04-15 00:50:24 +0100

    Printed card

--- components/projects/ProjectPrinted.vue
+++ components/projects/ProjectPrinted.vue
@@ -1,6 +1,6 @@
 <template>
   <Project :delay="500" url="https://www.printed.com">
-    <img src="/images/projects/printed.png" alt="Printed.com Logo" h="30" rounded="2xl" m="b-4 b-8" display="none md:block">
+    <img src="/images/projects/printed.png" alt="Printed.com Logo" h="22 md:30" rounded="2xl" m="b-4">
 
     <div>
       <h2 text="3xl md:5xl white" font="bold" m="b-4">
