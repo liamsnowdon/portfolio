c6dee061115e77e3e05bad6328a9151bfd1c668d
Liam Snowdon <snowdon.liam@gmail.com>
2023-11-04 00:22:03 +0000

    Responsive

--- components/projects/ProjectPrinted.vue
+++ components/projects/ProjectPrinted.vue
@@ -1,12 +1,14 @@
 <template>
   <Project :delay="400" url="https://www.printed.com">
-    <img src="/images/projects/printed.png" alt="Printed.com Logo" h="22 md:30" rounded="2xl" m="b-4">
+    <div flex="~" items="center" space="x-4 md:x-6">
+      <img src="/images/projects/printed.png" alt="Printed.com Logo" h="22 md:25 lg:30" rounded="2xl" m="b-4">
 
-    <div>
       <h2 text="3xl md:5xl white" font="bold" m="b-4">
         The Printed Promise
       </h2>
+    </div>
 
+    <div>
       <p text="lg neutral-400">
         With a wide range of products, Printed.com is the largest and fastest growing online print shop in the UK.
       </p>
