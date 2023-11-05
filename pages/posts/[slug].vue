40ca66074edfb39309930ba5c1d1bbb9da17d167
Liam Snowdon <snowdon.liam@gmail.com>
2023-11-05 02:16:42 +0000

    Revert "Use renderer directly"
    This reverts commit ce18acbd8084f24f6a081e5bbea86d19990809b3.

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -8,7 +8,7 @@ const { data: post } = await useAsyncData('post', () => {
 })
 
 useHead({
-  title: `${post.value?.title} - Liam Snowdon`,
+  titleTemplate: '%s - Liam Snowdon',
   meta: [
     {
       name: 'description',
@@ -54,41 +54,44 @@ useHead({
         <span>Back to posts</span>
       </NuxtLink>
 
-      <ContentRenderer v-if="post" :value="post">
-        <article>
-          <div m="b-8">
-            <h1 text="4xl md:6xl white" font="bold" m="b-4">
-              {{ post.title }}
-            </h1>
-            <p>{{ post.readingTime.text }} • Posted on {{ $dayjs(post.posted_at).format('MMM DD, YYYY') }}</p>
+      <ContentDoc>
+        <template #default="{ doc: post }">
+          <article>
+            <div m="b-8">
+              <h1 text="4xl md:6xl white" font="bold" m="b-4">
+                {{ post.title }}
+              </h1>
+              <p>{{ post.readingTime.text }} • Posted on {{ $dayjs(post.posted_at).format('MMM DD, YYYY') }}</p>
+            </div>
+            <ContentRenderer :value="post" />
+          </article>
+        </template>
+
+        <template #not-found>
+          <div
+            flex="~ col md:row"
+            justify="center"
+            items="center"
+            text="center md:left"
+            space="y-4 md:x-2 md:y-0"
+          >
+            <div flex="none">
+              <img src="/images/not-found-memoji.png" alt="Shrugging Liam Snowdon Memoji" h="30 md:60" w="30 md:60">
+            </div>
+
+            <div space="y-4">
+              <h2 text="3xl white" font="bold">
+                Well this is awkward...
+              </h2>
+              <p>
+                I couldn't find a post here, let's head <NuxtLink to="/posts" class="text-indigo-500 hover:text-indigo-400">
+                  back
+                </NuxtLink>.
+              </p>
+            </div>
           </div>
-          <ContentRendererMarkdown :value="post" />
-        </article>
-      </ContentRenderer>
-
-      <div
-        v-else
-        flex="~ col md:row"
-        justify="center"
-        items="center"
-        text="center md:left"
-        space="y-4 md:x-2 md:y-0"
-      >
-        <div flex="none">
-          <img src="/images/not-found-memoji.png" alt="Shrugging Liam Snowdon Memoji" h="30 md:60" w="30 md:60">
-        </div>
-
-        <div space="y-4">
-          <h2 text="3xl white" font="bold">
-            Well this is awkward...
-          </h2>
-          <p>
-            I couldn't find a post here, let's head <NuxtLink to="/posts" class="text-indigo-500 hover:text-indigo-400">
-              back
-            </NuxtLink>
-          </p>
-        </div>
-      </div>
+        </template>
+      </ContentDoc>
     </WrapperContent>
   </Wrapper>
 </template>
