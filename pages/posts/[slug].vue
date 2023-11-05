b1470eda5866291501577b16e060511932adfc73
Liam Snowdon <snowdon.liam@gmail.com>
2023-11-05 01:44:00 +0000

    Post meta data

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,14 +1,48 @@
 <script setup lang="ts">
-// const { data: post } = await useAsyncData('posts', () => {
-//   return queryContent('posts')
-//     .where({
-//       slug: useRoute().params.slug,
-//     })
-//     .findOne()
-// })
+const { data: post } = await useAsyncData('posts', () => {
+  return queryContent('posts')
+    .where({
+      slug: useRoute().params.slug,
+    })
+    .findOne()
+})
 
 useHead({
   titleTemplate: '%s - Liam Snowdon',
+  meta: [
+    {
+      name: 'description',
+      content: post.value?.intro,
+    },
+    {
+      name: 'og:title',
+      content: `${post.value?.title} - Liam Snowdon`,
+    },
+    {
+      name: 'og:description',
+      content: post.value?.intro,
+    },
+    {
+      name: 'og:type',
+      content: 'article',
+    },
+    {
+      name: 'article:published_time',
+      content: post.value?.posted_at,
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:title',
+      content: `${post.value?.title} - Liam Snowdon`,
+    },
+    {
+      name: 'twitter:description',
+      content: post.value?.intro,
+    },
+  ],
 })
 </script>
 
@@ -34,7 +68,28 @@ useHead({
         </template>
 
         <template #not-found>
-          <h1>Document not found</h1>
+          <div
+            flex="~ col md:row"
+            justify="center"
+            items="center"
+            text="center md:left"
+            space="y-4 md:x-2 md:y-0"
+          >
+            <div flex="none">
+              <img src="/images/not-found-memoji.png" alt="Shrugging Liam Snowdon Memoji" h="30 md:60" w="30 md:60">
+            </div>
+
+            <div space="y-4">
+              <h2 text="3xl white" font="bold">
+                Well this is awkward...
+              </h2>
+              <p>
+                I couldn't find a post here, let's head <NuxtLink to="/posts" class="text-indigo-500 hover:text-indigo-400">
+                  back
+                </NuxtLink>.
+              </p>
+            </div>
+          </div>
         </template>
       </ContentDoc>
     </WrapperContent>
